ea25a5a97f919e0bd763bd9d173567ada1fc1430
henzyd <henzydee@gmail.com>
2022-09-15 10:35:41 +0100

    i added a render()

--- my_venv/my_project/my_app/views.py
+++ my_venv/my_project/my_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_page(request):
-    return
+def home_page_func(request):
+    return render(request, 'my_app/home_page.html')
